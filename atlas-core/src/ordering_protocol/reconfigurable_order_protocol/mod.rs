c1d22b9cfc15211db9c688f32d8888a5c4625d29
Nuno Neto <nuno.neto.g@gmail.com>
2023-07-20 17:29:00 +0100

    Added one more phase (Which is an alternative to the normal stop and not another stage).

--- atlas-core/src/ordering_protocol/reconfigurable_order_protocol/mod.rs
+++ atlas-core/src/ordering_protocol/reconfigurable_order_protocol/mod.rs
@@ -1,16 +1,18 @@
 use atlas_common::error::*;
 use crate::reconfiguration_protocol::{QuorumJoinCert, ReconfigurationProtocol};
+use crate::serialize::ReconfigurationProtocolMessage;
 
 pub enum ReconfigurationAttemptResult {
     Failed,
+    AlreadyPartOfQuorum,
     InProgress,
     Successful,
 }
 
 /// The trait that defines the necessary operations for a given ordering protocol to be reconfigurable
-pub trait ReconfigurableOrderProtocol<RP, NT> where RP: ReconfigurationProtocol {
+pub trait ReconfigurableOrderProtocol<RP, NT> where RP: ReconfigurationProtocolMessage {
 
     /// Attempt to finalize a network view change which has been requested by us.
-    fn attempt_network_view_change(&mut self, join_certificate: QuorumJoinCert<RP::Serialization>) -> Result<ReconfigurationAttemptResult>;
+    fn attempt_network_view_change(&mut self, join_certificate: QuorumJoinCert<RP>) -> Result<ReconfigurationAttemptResult>;
 
 }
