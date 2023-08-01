64ad946c46c75340c5009f26332f875e400bdf5b
Nuno Neto <nuno.neto.g@gmail.com>
2023-08-01 16:52:44 +0100

    Fixed bugs with initialization of the quorum view change protocol, specially in where the stop quorum join message is bcast.

--- atlas-core/src/ordering_protocol/reconfigurable_order_protocol/mod.rs
+++ atlas-core/src/ordering_protocol/reconfigurable_order_protocol/mod.rs
@@ -11,7 +11,7 @@ pub enum ReconfigurationAttemptResult {
 
 /// The trait that defines the necessary operations for a given ordering protocol to be reconfigurable
 pub trait ReconfigurableOrderProtocol<RP> where RP: ReconfigurationProtocolMessage {
-
+    
     /// Attempt to finalize a network view change which has been requested by us.
     fn attempt_network_view_change(&mut self, join_certificate: QuorumJoinCert<RP>) -> Result<ReconfigurationAttemptResult>;
 
